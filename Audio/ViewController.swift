87716b9b5a1ffca233d469c7081519da49647834
doomout <winquake3@hanmail.net>
2022-12-26 22:26:07 +0900

    오디오 재생을 위한 초기화, readme update

--- Audio/ViewController.swift
+++ Audio/ViewController.swift
@@ -1,7 +1,13 @@
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVAudioPlayerDelegate {
 
+    var audioPlayer: AVAudioPlayer!
+    var audioFile:URL!
+    var MAX_VOLUME: Float = 10.0
+    var progressTimer : Timer!
+    
     @IBOutlet var pvProgressPlay: UIProgressView!
     @IBOutlet var lblCurrentTime: UILabel!
     @IBOutlet var lblEndTime: UILabel!
@@ -15,6 +21,22 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        audioFile = Bundle.main.url(forResource: "Sicilian_Breeze", withExtension: "mp3")
+        initPlay()
+    }
+    func initPlay() {
+        do{
+            audioPlayer = try AVAudioPlayer(contentsOf: audioFile)
+        } catch let error as NSError {
+            print("Error-initPlay : \(error)")
+        }
+        slVolume.maximumValue = MAX_VOLUME  //슬라이더의 최대 볼륭음 상수 10.0으로 초기화 한다.
+        slVolume.value = 1.0 //슬라이더의 볼륨을 1.0으로 초기화 한다
+        pvProgressPlay.progress = 0 //프로그래스 뷰의 진행을 0으로 초기화 한다.
+        
+        audioPlayer.delegate = self 
+        audioPlayer.prepareToPlay()
+        audioPlayer.volume = slVolume.value
     }
 
     @IBAction func btnPlayAudio(_ sender: UIButton) {
--- README.md
+++ README.md
@@ -3,3 +3,18 @@ AVAudioPlay 란?
 오디오 재생을 위한 앱    
 1. 지원하는 포멧  
 ACC, HE-ACC, Linear PCM, ALAC, AMR, iLBC, MP3
+
+2. do - try - catch 문   
+오류가 발생할 수 있는 함수를 호출 할 때 에러 처리를 위한 문 
+(사용 형식)     
+do {    
+    try 오류 발생 가능 코드 
+    오류가 발생하지 않을 시 실행할 코드    
+} catch 오류 패턴 1 {   
+    처리 구문   
+} catch 오류 패턴 2 where 추가 조건 {   
+    처리 구문   
+} catch {   
+    처리 구문   
+}   
+오류 타입은 한개 또는 그 이상도 가능. 
