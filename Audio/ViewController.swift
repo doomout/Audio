9b5a56fbffc304d1340c744bdc8319ee2a3780a9
doomout <winquake3@hanmail.net>
2022-12-26 20:56:21 +0900

    아웃렛 변수, 액션 함수 구현

--- Audio/ViewController.swift
+++ Audio/ViewController.swift
@@ -1,19 +1,33 @@
-//
-//  ViewController.swift
-//  Audio
-//
-//  Created by 현구김 on 2022/12/26.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var pvProgressPlay: UIProgressView!
+    @IBOutlet var lblCurrentTime: UILabel!
+    @IBOutlet var lblEndTime: UILabel!
+    
+    @IBOutlet var btnPlay: UIButton!
+    @IBOutlet var btnPause: UIButton!
+    @IBOutlet var btnStop: UIButton!
+    
+    @IBOutlet var slVolume: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func btnPlayAudio(_ sender: UIButton) {
+    }
+    
+    @IBAction func btnPauseAudio(_ sender: UIButton) {
+    }
+    
+    @IBAction func btnStopAudio(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func slChangeVolume(_ sender: UISlider) {
+    }
 }
 
